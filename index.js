3bf362cc4e38ae36ce11d2e9d21b980cb31b1954
Sarah Kim <sarahkstudio@users.noreply.github.com>
2020-07-19 22:36:30 -0700

    Basic working structure.

--- index.js
+++ index.js
@@ -1,3 +1,45 @@
-let myModel = new model();
+doTheWork();
 
-console.log(myModel.getListOfClasses());
+
+
+// populateClassesFromAPI()  // gives me a promise because of the async
+//     .then(classesFromAPI => {
+//         console.log(classesFromAPI);
+//
+//     })
+
+
+
+async function doTheWork() {
+    let classesFromAPI = await doAPIrequest('classes/');
+    console.log(classesFromAPI);
+
+    let arrayOfNames = classesFromAPI.results.map(current => current.index);
+    console.log(arrayOfNames);
+
+    // let finalString = await doAP
+    let allMyPromises = arrayOfNames.map(current => doAPIrequest(`/classes/${current}`));
+
+    const classDetails = await Promise.all(allMyPromises);
+    console.log(classDetails);
+}
+
+
+
+async function doAPIrequest(endpoint) {
+    let requestUrl = 'https://www.dnd5eapi.co/api/' + endpoint;
+    console.log('Fetching: ' + requestUrl);
+
+
+    let response = await fetch(requestUrl)
+
+    // Get the API response as a JSON.
+    const json = await response.json();
+    return json;
+}
+
+
+
+// async function makeAnArray(inputJSON) {
+//     return inputJSON.results.map(current => current.index)
+// }
