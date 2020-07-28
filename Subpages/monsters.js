b43091d5293d8f62e7e960924500de41ba1434f2
LiamRotchford5671 <liamrotchford@gmail.com>
2020-07-27 17:52:37 -0700

    Implemented expanding rows -utilized a codepen for expanding rows https://codepen.io/Sp00ky/pen/akVkyV -implented the codepen structure into monster.js -table structure is still built via react -table builds and loads all monster names, on click the toggle will expand the row by one with a "data" placeholder -imported in the css styles given on codepen that allow it to display correctly

--- Subpages/monsters.js
+++ Subpages/monsters.js
@@ -14,9 +14,9 @@ class Monsters extends React.Component {
   //Render elements to DOM.
   render() {
     return (
-      <table className="table table-hover">
+      <table className="table table-hover table-bordered">
         <thead className="thead-dark">
-          <tr>
+          <tr className="header">
             <th>Name</th>
           </tr>
         </thead>
@@ -29,7 +29,7 @@ class Monsters extends React.Component {
 async function start() {
   //Api request for classes.
   let monstersFromAPI = await doAPIrequest("monsters/");
-  console.log(monstersFromAPI);
+  //console.log(monstersFromAPI);
 
   let arrayOfMonsterNames = monstersFromAPI.results.map(
     (current) => current.index
@@ -37,6 +37,20 @@ async function start() {
 
   ReactDOM.render(<Monsters />, document.getElementById("dataTable"));
   addRow(arrayOfMonsterNames);
+
+  //toggle the nested expanding row
+  var ua = navigator.userAgent,
+    event = ua.match(/iPad/i) ? "touchstart" : "click";
+  if ($(".table").length > 0) {
+    $(".table .header").on(event, function () {
+      $(this)
+        .toggleClass("active", "")
+        .nextUntil(".header")
+        .css("display", function (i, v) {
+          return this.style.display === "table-row" ? "none" : "table-row";
+        });
+    });
+  }
 }
 
 //Run asynchronous api request and return in json format.
@@ -56,7 +70,12 @@ function addRow(arrayOfMonsterNames) {
 
   arrayOfMonsterNames.forEach((element) => {
     let newRow = document.createElement("tr");
+    newRow.className = "header";
     newRow.innerHTML = "<td>" + element + "</td>";
     bodyRef.appendChild(newRow);
+
+    let newNestedRow = document.createElement("tr");
+    newNestedRow.innerHTML = "<td>data</td>";
+    bodyRef.appendChild(newNestedRow);
   });
 }
