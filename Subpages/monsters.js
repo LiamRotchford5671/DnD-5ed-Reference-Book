03ac154016799f20a3300f63f8c170b98e7f3a68
LiamRotchford5671 <liamrotchford@gmail.com>
2020-07-29 18:36:08 -0700

    Implemented API endpoint call for individual monster data - Added in timeout Promise in new async function to gather monster data - Function will wait 4 seconds to gather all data of each monster pushed into an array of objects - Once all 322 items have been collected it will return back the data - Might consider that on table row click maybe it would be better to collect on click basis

--- Subpages/monsters.js
+++ Subpages/monsters.js
@@ -37,6 +37,40 @@ async function start() {
 
   ReactDOM.render(<Monsters />, document.getElementById("dataTable"));
   addRow(arrayOfMonsterNames);
+}
+
+//Run asynchronous api request and return in json format.
+async function doAPIrequest(endpoint) {
+  let requestUrl = "https://www.dnd5eapi.co/api/" + endpoint;
+  //console.log("Fetching: " + requestUrl);
+
+  let response = await fetch(requestUrl);
+
+  // Get the API response as a JSON.
+  const json = await response.json();
+  return json;
+}
+
+async function addRow(arrayOfMonsterNames) {
+  let bodyRef = document.getElementById("tableBody");
+  let monsterData = await gatherData(arrayOfMonsterNames).catch((err) => {
+    console.error(err);
+  });
+
+  //console.log(monsterData);
+
+  arrayOfMonsterNames.forEach((element) => {
+    let newRow = document.createElement("tr");
+    newRow.className = "header";
+    var nameCapitalized = element.charAt(0).toUpperCase() + element.slice(1);
+    newRow.innerHTML = "<td>" + nameCapitalized + "</td>";
+    bodyRef.appendChild(newRow);
+
+    let newNestedRow = document.createElement("tr");
+
+    newNestedRow.innerHTML = "<div>" + "data" + "</div>";
+    bodyRef.appendChild(newNestedRow);
+  });
 
   //toggle the nested expanding row
   var ua = navigator.userAgent,
@@ -53,30 +87,17 @@ async function start() {
   }
 }
 
-//Run asynchronous api request and return in json format.
-async function doAPIrequest(endpoint) {
-  let requestUrl = "https://www.dnd5eapi.co/api/" + endpoint;
-  //console.log("Fetching: " + requestUrl);
+function gatherData(arrayOfMonsterNames) {
+  return new Promise((resolve) => {
+    let dataArr = [];
+    arrayOfMonsterNames.forEach(async function (element) {
+      let endpoint = "monsters/" + element;
+      var data = await doAPIrequest(endpoint);
+      dataArr.push(data);
+    });
 
-  let response = await fetch(requestUrl);
-
-  // Get the API response as a JSON.
-  const json = await response.json();
-  return json;
-}
-
-function addRow(arrayOfMonsterNames) {
-  let bodyRef = document.getElementById("tableBody");
-
-  arrayOfMonsterNames.forEach((element) => {
-    let newRow = document.createElement("tr");
-    newRow.className = "header";
-    var nameCapitalized = element.charAt(0).toUpperCase() + element.slice(1);
-    newRow.innerHTML = "<td>" + nameCapitalized + "</td>";
-    bodyRef.appendChild(newRow);
-
-    let newNestedRow = document.createElement("tr");
-    newNestedRow.innerHTML = "<td>data</td>";
-    bodyRef.appendChild(newNestedRow);
+    setTimeout(() => {
+      resolve(dataArr);
+    }, 4000);
   });
 }
