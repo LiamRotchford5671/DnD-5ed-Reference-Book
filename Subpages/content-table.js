1eba058a02231fb921d574fd9a4afc207979cdaa
LiamRotchford5671 <liamrotchford@gmail.com>
2020-08-03 18:50:43 -0700

    Added in all equipment photos - also adjusted the filter to work on all 3 table page types

--- Subpages/content-table.js
+++ Subpages/content-table.js
@@ -2,7 +2,7 @@ function filterTable() {
   var input, filter, table, tr, td, i, txtValue;
   input = document.getElementById("filterInput");
   filter = input.value.toUpperCase();
-  table = document.getElementById("monsterTable");
+  table = document.getElementById("dataTable");
   tr = table.getElementsByTagName("tr");
   for (i = 0; i < tr.length; i++) {
     td = tr[i].getElementsByTagName("td")[0];
